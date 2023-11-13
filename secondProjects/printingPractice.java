67999372f8e6fd98bbb25078331a52a7dd035d41
Weirdchild24 <150052199+Weirdchild24@users.noreply.github.com>
2023-11-13 14:34:50 +0000

    jbh uvgctyrxd

---
+++ secondProjects/printingPractice.java
@@ -0,0 +1,12 @@
+class printingPractice {
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to My CS1 Project!");
+        System.out.println();
+        System.out.println("Dev's Name: Julianne Williams");
+        System.out.println();
+        System.out.println("Dev's Age: 15");
+        System.out.println();
+        System.out.println("Thank you for using this project!");
+    }
+}
---
+++ secondProjects/shortStory.java
@@ -0,0 +1,33 @@
+class SimpleStory {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time, in a small village,");
+        System.out.println("there lived a curious boy named Jack.");
+        
+        System.out.println("Jack discovered an old map in his attic,");
+        System.out.println("leading to a hidden treasure in the nearby forest.");
+
+        System.out.println("Excited, Jack embarked on a journey,");
+        System.out.println("braving the mysterious woods and overcoming challenges.");
+
+        System.out.println("Amidst the tall trees and trickling streams,");
+        System.out.println("Jack finally found the hidden treasure chest.");
+
+        System.out.println("To his surprise, the chest contained not gold,");
+        System.out.println("but a magical artifact that granted him a single wish.");
+
+        System.out.println("Jack thought for a moment and wished for,");
+        System.out.println("happiness to be brought to his entire village.");
+
+        System.out.println("The next day, the village was filled with joy,");
+        System.out.println("thanks to Jack's selfless wish.");
+
+        System.out.println("And so, Jack's simple act of kindness,");
+        System.out.println("turned his ordinary village into a place of everlasting happiness.");
+    }
+}
+
+
+
+
+
+
