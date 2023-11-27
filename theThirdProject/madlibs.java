c8d007f3346cafc550a69ed89506af4b32be8f46
Weirdchild24 <150052199+Weirdchild24@users.noreply.github.com>
2023-11-27 15:17:53 +0000

    jnibhyvucti

--- theThirdProject/madlibs.java
+++ theThirdProject/madlibs.java
@@ -13,15 +13,15 @@ class MadLibs {
 
         //////////////////////////////
         System.out.println("Welcome to Mad Libs!");
-        System.out.println("Let's create a fun story together.");
-
+        System.out.println("Let's create a fun story together!");
+        System.out.println(" ");
         System.out.println("Once upon a time, there was a " + adjective1 + " " + noun1 + ".");
         System.out.println("This " + noun1 + " loved to " + verb1 + " " + adverb + " every day.");
         System.out.println("One day, a " + adjective2 + " " + noun2 + " appeared!");
         System.out.println("The " + noun2 + " challenged our hero to " + verb2 + " " + adjective3 + ".");
         System.out.println("Despite the odds, our " + adjective1 + " " + noun1 + " triumphed and found a hidden " + noun3 + ".");
         System.out.println("In the end, they exclaimed, "+ exclamation);
-
+        System.out.println(" ");
         System.out.println("Thanks for playing Mad Libs!");
     }
 }
