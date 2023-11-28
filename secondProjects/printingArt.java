c310f5c93d549b8a37578242a3172516cbf1d0bf
Weirdchild24 <150052199+Weirdchild24@users.noreply.github.com>
2023-11-28 14:34:45 +0000

    imma go nom

---
+++ secondProjects/printingArt.java
@@ -0,0 +1,13 @@
+class printingArt
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Welcome to My Art Project!");
+        System.out.println(" ");
+        System.out.println("  /\\_/\\");
+        System.out.println(" ( o.o )");
+        System.out.println("  > ^ < ");
+        System.out.println(" ");
+        System.out.println("Thank you for exploring my artwork!");
+    }
+}
