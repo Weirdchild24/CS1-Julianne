d25b792300442c360a0b63cd663ef96d27bf926c
Weirdchild24 <150052199+Weirdchild24@users.noreply.github.com>
2023-12-13 15:19:32 +0000

    qwertyuio

---
+++ secondProjects/numberPartternPrinter.java
@@ -0,0 +1,12 @@
+class NumberPatternPrinter 
+{
+    public static void main(String args[]) 
+    {
+        System.out.println("1");
+        System.out.println("2 3");
+        System.out.println("4 5 6");
+        System.out.println("7 8 9 10");
+        System.out.println();
+        System.out.println("Thank you!");
+    }
+}
