8acae9850a9835f5b408d33448f7ee58187f839c
Weirdchild24 <150052199+Weirdchild24@users.noreply.github.com>
2024-01-09 15:10:40 +0000

    nbuyvctrx

---
+++ numberProjects/wholeDiv.java
@@ -0,0 +1,12 @@
+public class wholeDiv {
+    public static void main(String[] args) {
+        int dividend = 85;
+        int divisor = 9;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
